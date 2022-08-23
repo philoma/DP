4966599525de4d4fcd417e01025d5161638f6ee1
Mohd Faisal <faisal02v@gmail.com>
2022-08-23 22:50:26 +0530

    Update Fibonacci_series.cpp

--- Fibonacci_series.cpp
+++ Fibonacci_series.cpp
@@ -25,3 +25,19 @@ int main()
     }
     cout<<f(n,dp);
 }
+
+
+// //using bottom-up/tabulation method
+// class Solution {
+// public:
+//     int fib(int n) {
+//         int dp[n+1];
+//         dp[0]=0;
+//         if(n>=1)
+//             {dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }}
+//         return dp[n];        
+//     }
+// };
