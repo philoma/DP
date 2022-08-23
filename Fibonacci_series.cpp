92ed73cc49311cff5805a46dfc66776616d1ea62
Mohd Faisal <faisal02v@gmail.com>
2022-08-23 22:59:36 +0530

    Update Fibonacci_series.cpp

--- Fibonacci_series.cpp
+++ Fibonacci_series.cpp
@@ -41,3 +41,24 @@ int main()
 //         return dp[n];        
 //     }
 // };
+
+
+
+
+//just variable ka kheil
+class Solution {
+public:
+    int fib(int n) {
+        int prev1=0;
+        int prev2=1;
+        if(n==0||n==1)
+            return n;
+        int cur;
+        for(int i=2;i<=n;i++){
+            cur=prev1+prev2;
+            prev1=prev2;
+            prev2=cur;
+        }
+        return cur;
+    }
+};
